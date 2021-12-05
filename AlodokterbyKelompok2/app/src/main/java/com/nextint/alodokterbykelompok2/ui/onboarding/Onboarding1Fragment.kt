059ee541b731d3c36067f48387027d43e180e91a
itsannazzle <akarenina25@gmail.com>
2021-12-05 12:12:25 +0700

    onboarding screen

---
+++ AlodokterbyKelompok2/app/src/main/java/com/nextint/alodokterbykelompok2/ui/onboarding/Onboarding1Fragment.kt
@@ -0,0 +1,23 @@
+package com.nextint.alodokterbykelompok2.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nextint.alodokterbykelompok2.R
+
+
+
+class Onboarding1Fragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+    }
+
+}
