5ff40e3f8333e08067b59e9be14d12b8eeced988
itsannazzle <akarenina25@gmail.com>
2021-12-05 12:30:02 +0700

    splash

--- AlodokterbyKelompok2/app/src/main/java/com/nextint/alodokterbykelompok2/MainActivity.kt
+++ AlodokterbyKelompok2/app/src/main/java/com/nextint/alodokterbykelompok2/MainActivity.kt
@@ -2,10 +2,20 @@ package com.nextint.alodokterbykelompok2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.nextint.alodokterbykelompok2.ui.onboarding.Onboarding1Fragment
+import java.net.URLClassLoader.newInstance
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_AlodokterByKelompok2)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.appContainer, Onboarding1Fragment())
+                .commitNow()
+        }
         setContentView(R.layout.activity_main)
     }
 }
