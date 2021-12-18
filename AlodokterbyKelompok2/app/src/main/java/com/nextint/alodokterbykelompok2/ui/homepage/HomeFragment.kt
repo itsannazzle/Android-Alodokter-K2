137359ef94a3f77542ed0448343fbae3b9d622db
itsannazzle <akarenina25@gmail.com>
2021-12-18 12:38:17 +0700

    SessionManager

--- AlodokterbyKelompok2/app/src/main/java/com/nextint/alodokterbykelompok2/ui/homepage/HomeFragment.kt
+++ AlodokterbyKelompok2/app/src/main/java/com/nextint/alodokterbykelompok2/ui/homepage/HomeFragment.kt
@@ -9,27 +9,34 @@ import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.nextint.alodokterbykelompok2.R
 import com.nextint.alodokterbykelompok2.databinding.FragmentHomeBinding
 import com.nextint.alodokterbykelompok2.ui.article.ArticleAdapter
 import com.nextint.alodokterbykelompok2.ui.article.ArticleViewModel
 import com.nextint.alodokterbykelompok2.ui.doctor.DoctorAdapter
 import com.nextint.alodokterbykelompok2.ui.doctor.DoctorViewModel
+import com.nextint.alodokterbykelompok2.utils.SessionManager
+import com.nextint.alodokterbykelompok2.utils.SessionRepository
 import com.nextint.alodokterbykelompok2.viewmodel.ViewModelFactory
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    lateinit var sessionRepository: SessionRepository
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val sesi = SessionManager(requireContext())
+        sessionRepository = SessionRepository.getInstance(sesi)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.tvWelcomeUser.text = getString(R.string.selamat_datang, sessionRepository.getUsername())
         if (activity != null){
             val factory = ViewModelFactory.getInstance(requireActivity())
 
